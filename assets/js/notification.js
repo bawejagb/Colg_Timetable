0676d4a8b133d26d108d4f68d66e3d56e274551a
Gaurav <gaurav.baweja2508@gmail.com>
2020-10-01 08:53:16 +0530

    Revert "modification for Lecture only alerts and bug solved"
    This reverts commit 727bf88d8e6d86b75aef2f48e4d7fe2101ec6097.

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,14 +1,14 @@
 Notification.requestPermission().then(function(result) {
   });
   
-  var data =[[ [11,20,"DS Lecture","https://tiet.zoom.us/my/CSED15/"]],
-            [[9,40,"OS Lecture","https://tiet.zoom.us/my/CSED6/"],[10,30,"DMS Lecture","https://tiet.zoom.us/my/csed4/"],[11,20,"NA Lecture","https://tiet.zoom.us/my/tietsom6/"]],
+  var data =[[[8,50,"OS LAB","https://tiet.zoom.us/my/CSED6/"], [11,20,"DS Lecture","https://tiet.zoom.us/my/CSED15/"]],
+            [[9,40,"OS Lecture","https://tiet.zoom.us/my/CSED6/"],[10,30,"DMS Lecture","https://tiet.zoom.us/my/csed4/"],[11,20,"NA Lecture","https://tiet.zoom.us/my/tietsom6/"],[14,40,"DS LAB","https://tiet.zoom.us/my/csed5/"]],
             [[9,40,"CAO Lecture","https://tiet.zoom.us/my/csed11/"],[10,30,"NA Lecture","https://tiet.zoom.us/my/tietsom5/"],[11,20,"DS Lecture","https://tiet.zoom.us/my/CSED12/"],[16,20,"OS Lecture","https://tiet.zoom.us/my/CSED3/"]],
-            [[8,0,"DMS Lecture","https://tiet.zoom.us/my/csed2/"],[8,50,"PC Lecture","https://tiet.zoom.us/my/CSED16/"],[9,40,"CAO Lecture","https://tiet.zoom.us/my/csed10/"],[10,30,"NA Lecture","https://tiet.zoom.us/my/tietsom6/"]],
-            [[8,0,"DS Lecture","https://tiet.zoom.us/my/CSED2/"],[8,50,"DMS Lecture","https://tiet.zoom.us/my/csed1/"],[9,40,"OS Lecture","https://tiet.zoom.us/my/CSED2/"],[13,50,"CAO Lecture","https://tiet.zoom.us/my/csed4/"]]]; 
+            [[8,0,"DMS Lecture","https://tiet.zoom.us/my/csed2/"],[8,50,"PC Lecture","https://tiet.zoom.us/my/CSED16/"],[9,40,"CAO Lecture","https://tiet.zoom.us/my/csed10/"],[10,30,"NA Lecture","https://tiet.zoom.us/my/tietsom6/"],[14,40,"NA LAB","https://tiet.zoom.us/my/tietsom6/"],[16,20,"DMS LAB","https://tiet.zoom.us/my/csed4/"]],
+            [[8,0,"DS Lecture","https://tiet.zoom.us/my/CSED2/"],[8,50,"DMS Lecture","https://tiet.zoom.us/my/csed1/"],[9,40,"OS Lecture","https://tiet.zoom.us/my/CSED2/"],[12,10,"PC LAB","https://tiet.zoom.us/my/csed18/"],[13,50,"CAO Lecture","https://tiet.zoom.us/my/csed4/"]]]; 
   var flag = 0, d, m, day, h = 7;
   var check = function(){
-    if(h < 20){
+    if(h < 18){
       d = new Date();
       h = d.getHours();
       m = d.getMinutes();
@@ -17,7 +17,7 @@ Notification.requestPermission().then(function(result) {
       setTimeout(check, 5000);
     }
     else {
-        setTimeout(check, 5000);
+        setTimeout(check, 5000); // check again in a 2 second
     }
 }
 function NotfFunction(msg)
@@ -37,34 +37,17 @@ check();
    if ( day > 0 && day <= 5)
    {
       var data1 = data[day-1];
-      var hh1, mm1, hh2, mm2, cd;
       for(var i = 0; i < data1.length; i++)
       {
-        if ( data1[i][1] == 0)
-          {
-            mm1 = 55;
-            mm2 = 5;
-            hh1 = data1[i][0] -1;
-            hh2 = data1[i][0];
-            cd = (h == hh1 && m >= mm1 ) || (h == hh2 &&  m <= mm2);
-          }
-        else
-          {
-            mm1 = data1[i][1]-5;
-            mm2 = data1[i][1]+5;
-            hh1 = data1[i][0];
-            hh2 = data1[i][0];
-            cd = (h == hh1 && m >= mm1 ) && (h == hh2 &&  m <= mm2);
-          }
-        if(flag == 0 && cd)
+        if(h == data1[i][0] && m >= data1[i][1] - 5 && m <= data1[i][1] + 5 && flag == 0)
         {
+          console.log(data1[i][2] + " Start!");
           console.log(Notification.permission);
           if (Notification.permission === 'granted')
           {
-            console.log(data1[i][2] + " Start!");
             NotfFunction(data1[i]);
-            flag = 1;
           }
+          flag = 1;
         }
         else if (h == data1[i][0] && m > data1[i][1] + 6 && flag == 1)
         {
