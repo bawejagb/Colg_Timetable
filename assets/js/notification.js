23b3ee61e080d50758a4d66a742597774609cc6a
Gaurav <gaurav.baweja2508@gmail.com>
2021-02-23 23:02:24 +0530

    class time and link updated in js file

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,11 +1,11 @@
 Notification.requestPermission().then(function(result) {
   });
   var base = "https://tiet.zoom.us/my/"
-  var data =[[[8,50,"OS LAB","CSED6/"], [11,20,"DS Lecture","CSED15/"]],
-            [[9,40,"OS Lecture","CSED6/"],[10,30,"DMS Lecture","csed4/"],[11,20,"NA Lecture","tietsom6/"],[14,40,"DS LAB","csed5/"]],
-            [[9,40,"CAO Lecture","csed11/"],[10,30,"NA Lecture","tietsom5/"],[11,20,"DS Lecture","CSED12/"],[16,20,"OS Lecture","CSED3/"]],
-            [[8,0,"DMS Lecture","csed2/"],[8,50,"PC Lecture","CSED16/"],[9,40,"CAO Lecture","csed10/"],[10,30,"NA Lecture","tietsom6/"],[14,40,"NA LAB","tietsom6/"],[16,20,"DMS LAB","csed4/"]],
-            [[8,0,"DS Lecture","CSED2/"],[8,50,"DMS Lecture","csed1/"],[9,40,"OS Lecture","CSED2/"],[12,10,"PC LAB","csed18/"],[13,50,"CAO Lecture","csed4/"]]]; 
+  var data =[[[11,20,"Software Engineering LAB","CSED13/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED3/"], [15,30,"Artificial Intelligence LAB","CSED13/"]],
+            [[11,20,"Computer Networks LAB","CSED15/"], [13,50,"Optimization Techniques LEC","tietsom1/"], [14,40,"Computer Networks LEC","CSED14/"], [15,30,"Software Engineering LEC","CSED5/"], [16,20,"Design & Algorithms LEC","CSED9/"]],
+            [[13,00,"Artificial Intelligence LEC","CSED3/"], [13,50,"Optimization Techniques LEC","tietsom1/"], [14,40,"Design & Algorithms LEC","CSED16/"], [15,30,"Software Engineering LEC","CSED5/"]],
+            [[08,50,"Computer Networks LEC","CSED13/"], [09,40,"Design & Algorithms LEC","CSED14/"], [10,30,"Optimization Techniques LEC","tietsom2/"], [11,20,"Optimization Techniques LAB","tietsom2/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED16/"], [15,30,"Software Engineering LEC","CSED12/"]],
+            [[08,00,"Design & Algorithms LAB","CSED16/"],[09,40,"Database Management System LEC","CSED5/"],[12,10,"Database Management System LAB","CSED6/"]]]; 
   var flag = 0, d, m, day, h = 7;
   var check = function(){
     if(h < 18){
