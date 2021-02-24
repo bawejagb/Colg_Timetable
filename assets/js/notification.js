563adcf08a7314ab1404f38387d9b5ace8b73e94
Gaurav <gaurav.baweja2508@gmail.com>
2021-02-24 22:30:42 +0530

    API linked with notigication

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,11 +1,14 @@
+var data_bs = [],filter_data= [];
 $.ajax({
   url: 'https://sheetdb.io/api/v1/6tlzso8euumt3',
   type: "get",
   dataType: "json",
  
   success: function(data) {
+    data_bs = data;
     drawRow(data);
-    //console.log(data);
+    console.log(data);
+    check();
   }
 });
 var base = "https://tiet.zoom.us/my/"
@@ -32,13 +35,12 @@ function drawRow(data) {
   }
   
 Notification.requestPermission().then(function(result) {
-  });
-  
-  var data_bs =[[[11,20,"Software Engineering LAB","CSED13/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED3/"], [15,30,"Artificial Intelligence LAB","CSED13/"]],
-            [[11,20,"Computer Networks LAB","CSED15/"], [13,50,"Optimization Techniques LEC","tietsom1/"], [14,40,"Computer Networks LEC","CSED14/"], [15,30,"Software Engineering LEC","CSED5/"], [16,20,"Design & Algorithms LEC","CSED9/"]],
-            [[13,00,"Artificial Intelligence LEC","CSED3/"], [13,50,"Optimization Techniques LEC","tietsom1/"], [14,40,"Design & Algorithms LEC","CSED16/"], [15,30,"Software Engineering LEC","CSED5/"]],
-            [[08,50,"Computer Networks LEC","CSED13/"], [09,40,"Design & Algorithms LEC","CSED14/"], [10,30,"Optimization Techniques LEC","tietsom2/"], [11,20,"Optimization Techniques LAB","tietsom2/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED16/"], [15,30,"Software Engineering LEC","CSED12/"]],
-            [[08,00,"Design & Algorithms LAB","CSED16/"],[09,40,"Database Management System LEC","CSED5/"],[12,10,"Database Management System LAB","CSED6/"]]]; 
+  });  
+  function fitdata(day){
+    for(var i = 0; i < data_bs.length; i++){
+      filter_data[i] = data_bs[i][day];
+    }
+  }
   var flag = 0, d, m, day, h = 7;
   var check = function(){
     if(h < 18){
@@ -46,6 +48,8 @@ Notification.requestPermission().then(function(result) {
       h = d.getHours();
       m = d.getMinutes();
       day = d.getDay();
+      fitdata(day);
+      console.log(filter_data);
       checkAlert(h, m, day);
       setTimeout(check, 5000);
     }
@@ -53,42 +57,43 @@ Notification.requestPermission().then(function(result) {
         setTimeout(check, 5000); // check again in a 5 second
     }
 }
-function NotfFunction(msg)
+function NotfFunction(ref,msg)
 {
-  var nof = new Notification(msg[2], {
-    body : "AT: "+msg[0]+':'+msg[1]+"\nClick here: "+base+msg[3],
+  var nof = new Notification(msg[0], {
+    body : "AT: "+data_bs[ref]["Time"].split("-")[0]+"\nClick here: "+base+msg[1],
     icon : "assets/images/icon.ico"
   });
   nof.onclick = function(event) {
     event.preventDefault(); // prevent the browser from focusing the Notification's tab
-    window.open(base+msg[3], '_blank');
+    window.open(base+msg[1], '_blank');
   }
 }
-check();
+
  function checkAlert(h, m, day) {
   
    if ( day > 0 && day <= 5)
    {
-      var data1 = data_bs[day-1];
+      //var data1 = data_bs[day-1];
       var real_time = h*60 + m;
-      for(var i = 0; i < data1.length; i++)
+      for(var i = 0; i < filter_data.length; i++)
       {
-        var time_val = data1[i][0]*60 + data1[i][1];
-        if(real_time >= time_val - 5 && real_time <= time_val + 5 && flag == 0)
-        {
-          console.log(data1[i][2] + " Start!");
-          console.log(Notification.permission);
-          if (Notification.permission === 'granted')
+        if(filter_data[i].length > 3){
+          var time_val = (8*60)+i*50;
+          if(real_time >= time_val - 5 && real_time <= time_val + 5 && flag == 0)
           {
-            NotfFunction(data1[i]);
-            flag = 1;
+            console.log(filter_data[i].split(",")[0] + " Start!");
+            console.log(Notification.permission);
+            if (Notification.permission === 'granted')
+            {
+              NotfFunction(i,filter_data[i].split(","));
+              flag = 1;
+            }
           }
-          
-        }
-        else if (real_time >= time_val + 6 && real_time <= time_val + 8 && flag == 1)
-        {
-          flag = 0;
-          console.log(data1[i][2] + " Over!");
+          else if (real_time >= time_val + 6 && real_time <= time_val + 8 && flag == 1)
+          {
+            flag = 0;
+            console.log(filter_data[i].split(",")[0] + " Over!");
+          }
         }
       }
     }
