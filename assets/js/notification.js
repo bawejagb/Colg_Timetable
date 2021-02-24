b5d17232695904d46c42c54f06708180ea9f719c
Gaurav <gaurav.baweja2508@gmail.com>
2021-02-24 21:34:43 +0530

    API added for class link and github link added

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,7 +1,40 @@
+$.ajax({
+  url: 'https://sheetdb.io/api/v1/6tlzso8euumt3',
+  type: "get",
+  dataType: "json",
+ 
+  success: function(data) {
+    drawRow(data);
+    //console.log(data);
+  }
+});
+var base = "https://tiet.zoom.us/my/"
+function drawRow(data) {
+  for(var i = 0; i < data.length; i++){
+    var lst = data[i];
+    var row = $("<tr />")
+    $("#my-table").append(row); 
+    row.append($("<th id='colr' scope='row'>" + lst["Time"] + "</th>"));
+    for(var j = 1; j <= 5; j++){
+      var dat = lst[j];
+      if(dat == "-")
+        row.append($("<td id='colr' scope='row'>" + "-" + "</td>")); 
+      else if(dat.length > 2){
+        var dk = dat.split(",");
+        if(dk[0].search("LAB") == -1)
+        row.append($("<td class='table-warning'>" + "<a href="+base+dk[1]+" target='_blank'><b>"+dk[0]+"</b></a></td>")); 
+        else
+        row.append($("<td class='table-success' rowspan='2'style='vertical-align:middle'>" + "<a href="+base+dk[1]+" target='_blank'><b>"+dk[0]+"</b></a></td>"));
+      }
+      
+    }
+}
+  }
+  
 Notification.requestPermission().then(function(result) {
   });
-  var base = "https://tiet.zoom.us/my/"
-  var data =[[[11,20,"Software Engineering LAB","CSED13/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED3/"], [15,30,"Artificial Intelligence LAB","CSED13/"]],
+  
+  var data_bs =[[[11,20,"Software Engineering LAB","CSED13/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED3/"], [15,30,"Artificial Intelligence LAB","CSED13/"]],
             [[11,20,"Computer Networks LAB","CSED15/"], [13,50,"Optimization Techniques LEC","tietsom1/"], [14,40,"Computer Networks LEC","CSED14/"], [15,30,"Software Engineering LEC","CSED5/"], [16,20,"Design & Algorithms LEC","CSED9/"]],
             [[13,00,"Artificial Intelligence LEC","CSED3/"], [13,50,"Optimization Techniques LEC","tietsom1/"], [14,40,"Design & Algorithms LEC","CSED16/"], [15,30,"Software Engineering LEC","CSED5/"]],
             [[08,50,"Computer Networks LEC","CSED13/"], [09,40,"Design & Algorithms LEC","CSED14/"], [10,30,"Optimization Techniques LEC","tietsom2/"], [11,20,"Optimization Techniques LAB","tietsom2/"], [13,50,"Database Management System LEC","CSED13/"], [14,40,"Artificial Intelligence LEC","CSED16/"], [15,30,"Software Engineering LEC","CSED12/"]],
@@ -36,7 +69,7 @@ check();
   
    if ( day > 0 && day <= 5)
    {
-      var data1 = data[day-1];
+      var data1 = data_bs[day-1];
       var real_time = h*60 + m;
       for(var i = 0; i < data1.length; i++)
       {
